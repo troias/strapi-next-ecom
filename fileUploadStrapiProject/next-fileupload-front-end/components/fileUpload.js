663823e1fa5bfdd3ddae1563c23b7da6e81cf063
troy <hunter007nz@gmail.com>
2022-02-04 16:28:27 +1300

    first commit

---
+++ fileUploadStrapiProject/next-fileupload-front-end/components/fileUpload.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+ const FileUpload = () => {
+  return <div>
+      <div>
+    <input type="file" name="file" id="file" />
+    <label htmlFor="file">Choose a file</label>
+    </div>
+
+  </div>;
+};
+
+export default FileUpload;
